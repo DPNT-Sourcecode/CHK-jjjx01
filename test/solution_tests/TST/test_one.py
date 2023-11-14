e2e0e5c074934566824333e2024f6cd0b71b17e2
sbx_user1051 <sbx_user1051@169.254.46.213>
2023-11-14 08:39:22 +0000

    Tue Nov 14 08:39:22 UTC 2023

--- test/solution_tests/TST/test_one.py
+++ test/solution_tests/TST/test_one.py
@@ -1,6 +1,8 @@
-from solutions.TST import one
+from lib.solutions.TST import one
+from lib.solutions.CHK import checkout_solution
 
 
 class TestSum():
     def test_sum(self):
-        assert one.get() == 1
+        assert checkout_solution.checkout('AABCB') == 165
+
