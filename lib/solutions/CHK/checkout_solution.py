448451533ae03a6d9d1c50f2da7aaf30d3a55418
sbx_user1051 <sbx_user1051@169.254.46.213>
2023-11-14 08:26:56 +0000

    Tue Nov 14 08:26:56 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,11 +3,35 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    legal_inputs = {'A', 'B', 'C', 'D'}
+    price = 0
     basket_dict = {}
+
     for c in skus:
+        if c not in legal_inputs:
+            return -1
         if c in basket_dict.keys():
             basket_dict[c] += 1
         else:
             basket_dict[c] = 1
 
-    
+    if basket_dict['A'] // 3 >= 1:
+        price += (basket_dict['A'] // 3) * 130
+        price += (basket_dict['A'] % 3) * 50
+    else:
+        price += basket_dict['A'] * 50
+
+    if basket_dict['B'] // 2 >= 1:
+        price += (basket_dict['B'] // 2) * 45
+        price += (basket_dict['B'] % 2) * 30
+    else:
+        price += basket_dict['B'] * 30
+
+    if 'C' in basket_dict.keys():
+        price += basket_dict['C'] * 20
+    if 'D' in basket_dict.keys():
+        price += basket_dict['D'] * 15
+
+    return price
+
+
--- test/solution_tests/CHK_R1/test_chk_r1.py
+++ test/solution_tests/CHK_R1/test_chk_r1.py
@@ -4,3 +4,4 @@ from lib.solutions.CHK import checkout_solution
 class CheckTest():
     def test_check(self):
         assert checkout_solution.checkout('AABCB') == 165
+        assert checkout_solution.checkout('AABCBE') == -1
